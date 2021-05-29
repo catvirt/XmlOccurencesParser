e73d4ab906830715d0a0e9da89b89fc24801a89c
EbicHecker <60292167+epic6969@users.noreply.github.com>
2021-05-30 01:55:09 +0200

    noob ! >:))

--- EarthParser/EarthParser/Program.cs
+++ EarthParser/EarthParser/Program.cs
@@ -1,39 +1,39 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using System.Collections.Generic;
-using System.IO;
 
 namespace EarthParser
 {
-    class Program
+    internal static class Program
     {
         private static void Main(string[] arguments)
         {
-            if (arguments.Length == 0)
+            if (arguments.Length < 2 || arguments[0] == null && arguments[1] == null)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(" > Invalid Arguments!");
                 Console.ResetColor();
+                return;
             }
-            else if (arguments[0] != null && arguments[1] != null)
+            var xml = XDocument.Load(arguments[0]);
+            var root = xml.Root;
+            if (root == null)
             {
-                var xml = XDocument.Load(arguments[0]);
-
-                XElement root = xml.Root;
-
-                XNamespace xname = root.GetDefaultNamespace();
-
-                IEnumerable<XElement> KeysEntry = from key in xml.Descendants(xname + arguments[1]) select key;
-
-                foreach (var earthParse in KeysEntry)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(earthParse.Value);
-                    File.AppendAllText("OccurencesFound.txt", earthParse.Value);
-                    Console.ResetColor();
-                }
-            }          
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" > Root Cannot Be Null!");
+                Console.ResetColor();
+                return;
+            }
+            var xname = root.GetDefaultNamespace();
+            var KeysEntry = from key in xml.Descendants(xname + arguments[1]) select key;
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var earthParse in KeysEntry)
+            {
+                Console.WriteLine(earthParse.Value);
+                File.AppendAllText("FoundOccurrences.txt", $"{earthParse.Value}{Environment.NewLine}");
+            }
+            Console.ResetColor();
         }
     }
 }
