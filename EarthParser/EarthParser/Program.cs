4063cd9cedb2bac40a863f144311a55e9f71e938
catvirt <82500684+catvirt@users.noreply.github.com>
2021-05-29 22:45:02 +0300

    Add files via upload
    Usage: filename.extension occurences_to_p
    
    Example: https://media.discordapp.net/attachments/834706521198690327/848285765377196086/unknown.png

---
+++ EarthParser/EarthParser/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EarthParser
+{
+    class Program
+    {
+        private static void Main(string[] arguments)
+        {
+            if (arguments.Length == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" > Invalid Arguments!");
+                Console.ResetColor();
+            }
+            else if (arguments[0] != null && arguments[1] != null)
+            {
+                var xml = XDocument.Load(arguments[0]);
+
+                XElement root = xml.Root;
+
+                XNamespace xname = root.GetDefaultNamespace();
+
+                IEnumerable<XElement> KeysEntry = from key in xml.Descendants(xname + arguments[1]) select key;
+
+                foreach (var earthParse in KeysEntry)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(earthParse.Value);
+                    File.AppendAllText("OccurencesFound.txt", earthParse.Value);
+                    Console.ResetColor();
+                }
+            }          
+        }
+    }
+}
